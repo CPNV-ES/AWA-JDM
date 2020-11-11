c9e52686e10643a38c4b6462430bf166f161d106
Yvann <34337152+MrHD586@users.noreply.github.com>
2020-11-11 07:49:40 +0100

    Add display animation to car contents

--- js/home.js
+++ js/home.js
@@ -2,51 +2,50 @@
 // init controller
 var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
 
-//TweenMax.set("#car1-content", {opacity:0});
-TweenMax.set("#car2-content", {opacity:0});
-TweenMax.set("#car3-content", {opacity:0});
-TweenMax.set("#car4-content", {opacity:0});     
-
-
-var car1 = new TimelineLite();
-var $titre = $(".car-content");
-mySplitText = new SplitText($titre, {type:"words"});
-TweenLite.set($titre, {perspective:400});
-mySplitText.split({type:"words"});
-
-
-
-
 // Build scenes
  
 //Main wallpaper
- new ScrollMagic.Scene({triggerElement: "#home"})
-   .setTween("#home > div", {y: "80%", ease: Linear.easeNone})
-   .addTo(controller);
+new ScrollMagic.Scene({triggerElement: "#home"})
+  .setTween("#home > div", {y: "80%", ease: Linear.easeNone})
+  .addTo(controller);
 
 
- //Car 1
- new ScrollMagic.Scene({triggerElement: "#car1"})
-    .setTween("#car2 > div", {y: "80%", ease: Linear.easeNone})
-    .on('start', function(){
-        $(mySplitText.words).each(function(index,el){
-            car1.from($(el), .5, {opacity:0, force3D:true}, .5+index * 0.05);
-            car1.from($(el), .5, {scale:index % 2 == 0  ? 0 : 4, ease:Back.easeOut}, .5+index * 0.05);
-        });
-   })
-   .addTo(controller);
+//Car 1  
+new ScrollMagic.Scene({triggerElement: "#car1"})
+  .setTween("#car1 > div", {y: "80%", ease: Linear.easeNone})
+  .addTo(controller);
 
- //Car 2
- new ScrollMagic.Scene({triggerElement: "#car2"})
-   .setTween("#car2 > div", {y: "80%", ease: Linear.easeNone})
-   .addTo(controller);
+// Car 1 content
+new ScrollMagic.Scene({triggerElement: "#car1-content", triggerHook: 1, duration: "200%", offset: 530})
+.setClassToggle("#car1-content", "visible")  
+.addTo(controller);
 
- //Car 3
- new ScrollMagic.Scene({triggerElement: "#car3"})
-   .setTween("#car3 > div", {y: "80%", ease: Linear.easeNone})
-   .addTo(controller);
+//Car 2
+new ScrollMagic.Scene({triggerElement: "#car2"})
+  .setTween("#car2 > div", {y: "80%", ease: Linear.easeNone})
+  .addTo(controller);
 
- //Car 4
- new ScrollMagic.Scene({triggerElement: "#car4"})
-   .setTween("#car4 > div", {y: "80%", ease: Linear.easeNone})
-   .addTo(controller);
+// Car 2 content
+new ScrollMagic.Scene({triggerElement: "#car2-content", triggerHook: 1, duration: "200%", offset: 500})
+.setClassToggle("#car2-content", "visible") 
+.addTo(controller);
+
+//Car 3
+new ScrollMagic.Scene({triggerElement: "#car3"})
+  .setTween("#car3 > div", {y: "80%", ease: Linear.easeNone})
+  .addTo(controller);
+
+// Car 3 content
+new ScrollMagic.Scene({triggerElement: "#car3-content", triggerHook: 1, duration: "200%", offset: 560})
+.setClassToggle("#car3-content", "visible") 
+.addTo(controller);
+
+//Car 4
+new ScrollMagic.Scene({triggerElement: "#car4"})
+  .setTween("#car4 > div", {y: "80%", ease: Linear.easeNone})
+  .addTo(controller);
+
+// Car 4 content
+new ScrollMagic.Scene({triggerElement: "#car4-content", triggerHook: 1, duration: "200%", offset: 510})
+.setClassToggle("#car4-content", "visible") 
+.addTo(controller);
