91d991b55d1e676562338b4d2194eb7ef671a420
Killian Viquerat <killian.viquerat@cpnv.ch>
2020-11-11 10:07:38 +0100

    add commentaire

--- js/Cars_script.js
+++ js/Cars_script.js
@@ -8,16 +8,16 @@ const hover = gsap.to(cars, {
     paused: true,
     ease: "ease-in-out"
   });
-  
+
+//Déplacement des card de droite à gauche et change de l'opacité de 0 à 1
 HTMLCollection.prototype.forEach = Array.prototype.forEach;
 cars.forEach((element) => {
     gsap.set(element, {autoAlpha: 0})
     gsap.fromTo(element, {x: 1500}, {duration: 2, x: 0})
     gsap.to(element, {autoAlpha: 1 ,duration: 2})
-
     
 })
-
+//Changement de la scale des images
 cars.forEach((element) => {
     element.addEventListener("mouseenter",hover.play())
 })
